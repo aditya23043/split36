514a05159744f3c98202659dadf4dbca6b412c78
Aditya Gautam <adityascottish27@gmail.com>
2025-07-08 00:36:30 +0530

    qmk config finally donw

---
+++ src/split36/config.h
@@ -0,0 +1,9 @@
+// #define MATRIX_ROWS 4
+// #define MATRIX_COLS 5
+
+#define MASTER_LEFT
+
+// #define SERIAL_USART_FULL_DUPLEX
+// #define SERIAL_USART_DRIVER SIOD0
+// #define SERIAL_USART_TX_PIN GP0
+// #define SERIAL_USART_RX_PIN GP1
